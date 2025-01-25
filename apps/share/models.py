45ab72ed9b24c8f666df276100665f0a80d478c1
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2025-01-26 04:10:28 +0500

    any

--- apps/share/models.py
+++ apps/share/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from datetime import datetime, timedelta
 from django.utils import timezone
 import uuid
-import pytz
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
