0c4c6e01f3c2f8ca4744a57a27618972d6d55b5a
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2025-01-26 04:07:02 +0500

    any

--- apps/share/models.py
+++ apps/share/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime, timedelta
+from django.utils import timezone
 import uuid
 import pytz
 
@@ -33,7 +34,7 @@ class BaseMessageModel(models.Model):
 class BaseScheduledMessageModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField()
-    scheduled_time = models.DateTimeField()
+    scheduled_time =  models.DateTimeField(default=datetime.now() + timedelta(days=1))
     sent = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -41,5 +42,11 @@ class BaseScheduledMessageModel(models.Model):
     class Meta:
         abstract = True
 
+    # def save(self, *args, **kwargs):
+    #     # scheduled_time ni UTC ga aylantirish
+    #     # if not isinstance(self.scheduled_time, str):
+    #     if self.scheduled_time and self.scheduled_time.tzinfo is None:
+    #             self.scheduled_time = datetime(2025, 1, 26, 23, 1, 11, 553317)
+    #     super().save(*args, **kwargs)
 
 
