a19cfe1e1365ea3a2c1bd9645c42ca6558973a98
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2025-02-04 01:14:30 +0500

    any

--- core/settings.py
+++ core/settings.py
@@ -370,7 +370,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 # -----------------------------------------------------------------------------------------
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'http://localhost:9200',
+        'hosts': 'http://0.0.0.0:9200',
     },
 }
 
